1f5d727cd837a269e338d86bf4c0101fb86999fc
Raz Matzliah <razm1213@gmail.com>
2022-07-20 13:26:16 +0300

    final

--- src/RabbitMqPublisher.py
+++ src/RabbitMqPublisher.py
@@ -1,3 +1,5 @@
+import threading
+
 from configurations.RabbitMqPublisherConfigure import RabbitMqPublisherConfigure
 import pika
 
@@ -6,66 +8,18 @@ class RabbitMqPublisher:
     def __init__(self, server: RabbitMqPublisherConfigure):
         self.server = server
         self._connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.server.host))
-        self._channel = self._connection.channel()
-        self._channel.queue_declare(queue=self.server.queue)
+        self.channel = self._connection.channel()
+        self.channel.queue_declare(queue=self.server.queue)
 
     def publish(self, payload):
-        self._channel.basic_publish(
+        self.channel.basic_publish(
             exchange=self.server.exchange,
             routing_key=self.server.routingKey,
-            body=str(body)
+            body=str(payload)
         )
 
         print("Published Message:\n {}".format(payload))
-        self._connection.close()
-
-
-body = """{
-  "studentDetails": {
-    "firstName": "Raz",
-    "lastName": "Matzliah",
-    "id": 322717570
-  },
-  "subjectGrades": [
-    {
-      "subject": "Math",
-      "grades": [
-        100,
-        90,
-        96
-      ]
-    },
-    {
-      "subject": "English",
-      "grades": [
-        98,
-        95,
-        94
-      ]
-    },
-    {
-      "subject": "History",
-      "grades": [
-        100,
-        97,
-        85
-      ]
-    },
-    {
-      "subject": "Chemistry",
-      "grades": [
-        93,
-        90,
-        100
-      ]
-    }
-  ],
-  "birthDate": "27/06/2000",
-  "age": 22,
-  "gender": "זכר",
-  "behaviourGrade": 8
-}
-"""
+        # self._connection.close()
 
 
 if __name__ == "__main__":
@@ -75,6 +29,53 @@ if __name__ == "__main__":
         routingKey='student.data',
         exchange='student'
     )
+
+    body = """{
+      "studentDetails": {
+        "firstName": "Test",
+        "lastName": "Tester",
+        "id": 123456789
+      },
+      "subjectGrades": [
+        {
+          "subject": "Math",
+          "grades": [
+            100,
+            90,
+            96
+          ]
+        },
+        {
+          "subject": "English",
+          "grades": [
+            98,
+            95,
+            94
+          ]
+        },
+        {
+          "subject": "History",
+          "grades": [
+            100,
+            97,
+            85
+          ]
+        },
+        {
+          "subject": "Chemistry",
+          "grades": [
+            93,
+            90,
+            100
+          ]
+        }
+      ],
+      "birthDate": "01/01/2000",
+      "age": 22,
+      "gender": "זכר",
+      "behaviourGrade": 4
+    }
+    """
 
     rabbitmq = RabbitMqPublisher(server_configure)
     rabbitmq.publish(body)
